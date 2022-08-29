44fbf07bc69465b1b7b2244a1ba698352cb5ae7e
Luis Tovar <ltovar2@huskers.unl.edu>
2022-08-29 22:52:49 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Luis Tovar
+ *         ltovar2@huskers.unl.edu
+ * Date: 08/23/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Luis Tovar$\n");
+  printf("Electrical Engineering\n");
+
+  return 0;
+}
